370976f5fa06cac5d73cfc780104ed98233198aa
husnain haider <husnainhaider999@gmail.com>
2018-11-25 21:09:51 -0500

    Working marker placement and radius

--- web_components/imports/ui/body.js
+++ web_components/imports/ui/body.js
@@ -5,7 +5,7 @@ import './body.html';
 import './map.html'
 import './navigation.html';
 import { Patients } from "../api/patients";
-
+import geolib from 'geolib';
 
 Template.layout.onCreated(
     function bodyOnCreated(){
@@ -19,71 +19,98 @@ Template.layout.helpers({
     },
 });
 
-Template.map.onCreated(function() {
-    var self = this;
 
-    GoogleMaps.ready('map', function(map) {
-        var marker;
+    Template.map.onRendered(function() {
+        var self = this;
 
-        // Create and move the marker when latLng changes.
-        self.autorun(function() {
-            var latLng = Geolocation.latLng();
-            if (! latLng)
-                return;
+        GoogleMaps.ready('map', function (map) {
+            var marker;
+            var circle;
 
-            // If the marker doesn't yet exist, create it.
-            if (! marker) {
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(latLng.lat, latLng.lng),
-                    map: map.instance
+
+            // Create and move the marker when latLng changes.
+            self.autorun(function () {
+                var latLng = Geolocation.latLng();
+                if (!latLng)
+                    return;
+
+                // If the marker doesn't yet exist, create it.
+                //if (!marker) {
+
+
+                google.maps.event.addListener( map.instance, 'click', function(event) {
+                       // marker2 = new google.maps.Marker({
+                         //   position: event.latLng,
+                           // map: map.instance});
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(latLng.lat, latLng.lng),
+                        map: map.instance,
+                        draggable: true
+
+                    });
+                    marker.setPosition(event.latLng)
+                    console.log(event.latLng.lng());
+
+                    console.log(('Within Radius: ' + geolib.isPointInCircle({latitude:event.latLng.lat(),longitude:event.latLng.lng()}, {
+                        latitude: 44.227919299999996,
+                        longitude: -76.49169979999999
+                    }, 50)));
+                    circle = new google.maps.Circle({
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 0.8,
+                        strokeWeight: 2,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0.35,
+                        map: map.instance,
+                        center: event.latLng,
+                        radius: 50
+                    });
                 });
-            }
-            // The marker already exists, so we'll just change its position.
-            else {
-                marker.setPosition(latLng);
-            }
-
-            // Center and zoom the map view onto the current position.
-            map.instance.setCenter(marker.getPosition());
-            map.instance.setZoom(15);
+                //}
+                // The marker already exists, so we'll just change its position.
+                //else {
+                //    marker.setPosition(latLng);
+                }
+                //map.instance.setZoom(15);
+                /*function distance(position) {
+                    alert('Within Radius: ' + geolib.isPointInCircle(position.coords, {
+                        latitude: 51.525,
+                        longitude: 7.4575
+                    }, 300000));
+                }*/
+            );
         });
-    });
-});
 
-Template.map.helpers({
-    geolocationError: function() {
-        var error = Geolocation.error();
-        return error && error.message;
-    },
-    mapOptions: function() {
-        var latLng = Geolocation.latLng();
-        // Initialize the map once we have the latLng.
-        if (GoogleMaps.loaded() && latLng) {
-            return {
-                center: new google.maps.LatLng(latLng.lat, latLng.lng),
-                zoom: 15
-            };
+
+
+    });
+
+
+
+    Template.map.helpers({
+        geolocationError: function () {
+            var error = Geolocation.error();
+            return error && error.message;
+        },
+        mapOptions: function () {
+            var latLng = Geolocation.latLng();
+            // Initialize the map once we have the latLng.
+            if (GoogleMaps.loaded() && latLng) {
+                return {
+
+                    center: new google.maps.LatLng(latLng.lat, latLng.lng),
+                    zoom: 15
+                };
+            }
         }
-    }
-});
+
+
+    });
+
+    Router.route('/', function () {
+        this.render('layout');
+    })
     /*
     //THIS IS THE DISTANCE FUNCTION
- import geolib from 'geolib';
-navigator.geolocation.getCurrentPosition(
-    function (position) {
-        alert('Within Radius: ' + geolib.isPointInCircle(position.coords, {
-            latitude: 51.525,
-            longitude: 7.4575
-        }, 300));
-    },
-    function () {
-        alert('Position could not be determined.')
-    },
-    {
-        enableHighAccuracy: true
-    }
-)
+
 */
-Router.route('/', function(){
-    this.render('layout');
-});
